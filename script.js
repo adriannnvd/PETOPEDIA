2ccac010089f4a2fea11928f0394c93a6e708902
adriannnvd <adriandevilla0518@gmail.com>
2024-11-17 09:50:37 +0800

    connect main page and category page

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ document.addEventListener('wheel', function (event) {
         event.preventDefault(); // Prevent zooming
     }
 }, { passive: false });
+
+
+const exploreBtn = document.querySelector('.explore-btn');
+const exploreIcon = document.querySelector('.explore img');
+
+// Define original and hover images
+const newImageSrc = '/images/main/explore-icon-hover.png';
+const originalImageSrc = '/images/main/explore-icon.png';
+
+// Add hover event listeners
+exploreBtn.addEventListener('mouseover', () => {
+    exploreIcon.src = newImageSrc; // Change to hover image
+    exploreIcon.style.height = '120px'; // Increase size
+    exploreIcon.style.top = '-25px'; // Adjust position
+});
+
+exploreBtn.addEventListener('mouseout', () => {
+    exploreIcon.src = originalImageSrc; // Restore original image
+    exploreIcon.style.height = '100px'; // Restore size
+    exploreIcon.style.top = '0'; // Restore position
+});
