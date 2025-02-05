c02a798c75e21d4529022aa10e99a13b3c9ad82a
adriannnvd <adriandevilla0518@gmail.com>
2025-02-05 14:11:12 +0800

    allow zoom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-document.addEventListener('wheel', function (event) {
-    if (event.ctrlKey) {
-        event.preventDefault(); // Prevent zooming
-    }
-}, { passive: false });
+// document.addEventListener('wheel', function (event) {
+//     if (event.ctrlKey) {
+//         event.preventDefault(); // Prevent zooming
+//     }
+// }, { passive: false });
 
 
 const exploreBtn = document.querySelector('.explore-btn');
