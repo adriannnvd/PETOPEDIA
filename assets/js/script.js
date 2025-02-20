33b5bb84535d8b03733900f4868ebd1e5c45741d
adriannnvd <adriandevilla0518@gmail.com>
2025-02-20 17:34:54 +0800

    remove hover effect for explore btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,25 +5,25 @@ document.addEventListener('wheel', function (event) {
 }, { passive: false });
 
 
-const exploreBtn = document.querySelector('.explore-btn');
-const exploreIcon = document.querySelector('.explore img');
+// const exploreBtn = document.querySelector('.explore-btn');
+// const exploreIcon = document.querySelector('.explore img');
 
-// Define original and hover images
-const newImageSrc = 'assets/images/main/explore-icon-hover.png';
-const originalImageSrc = 'assets/images/main/explore-icon.png';
+// // Define original and hover images
+// const newImageSrc = 'assets/images/main/explore-icon-hover.png';
+// const originalImageSrc = 'assets/images/main/explore-icon.png';
 
-// Add hover event listeners
-exploreBtn.addEventListener('mouseover', () => {
-    exploreIcon.src = newImageSrc; // Change to hover image
-    exploreIcon.style.height = '120px'; // Increase size
-    exploreIcon.style.top = '-25px'; // Adjust position
-});
+// // Add hover event listeners
+// exploreBtn.addEventListener('mouseover', () => {
+//     exploreIcon.src = newImageSrc; // Change to hover image
+//     exploreIcon.style.height = '120px'; // Increase size
+//     exploreIcon.style.top = '-25px'; // Adjust position
+// });
 
-exploreBtn.addEventListener('mouseout', () => {
-    exploreIcon.src = originalImageSrc; // Restore original image
-    exploreIcon.style.height = '100px'; // Restore size
-    exploreIcon.style.top = '0'; // Restore position
-});
+// exploreBtn.addEventListener('mouseout', () => {
+//     exploreIcon.src = originalImageSrc; // Restore original image
+//     exploreIcon.style.height = '100px'; // Restore size
+//     exploreIcon.style.top = '0'; // Restore position
+// });
 
 
 
